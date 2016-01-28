a257983b341bff2689dc305a6283c6170da84294
Sirui <sirui.xing@gmail.com>
2016-01-28 00:20:40 -0600

    add monitor TODO: save data

--- com/sirui/sim/2DSim.py
+++ com/sirui/sim/2DSim.py
@@ -1,8 +1,7 @@
 from __future__ import print_function
 import random
 import simpy
-
-
+from com.sirui.sim.monitor import Monitor
 
 RANDOM_SEED = 42
 SCOPE_SIZE = 5
@@ -16,9 +15,10 @@ class Field(object):
         self.locations = [simpy.Resource(env) for i in range(scope_size)]
 
 class Atom(object):
-    def __init__(self, name, field, position=None):
-        self.name = name
+    def __init__(self, id, field, monitor, position=None):
+        self.id = id
         self.field = field
+        self.monitor = monitor
         if position is None:
             # generate initial position using collision avoidance
             while True:
@@ -59,46 +59,46 @@ class Atom(object):
         return self.field.locations[left_position].count + self.field.locations[right_position].count
     def run(self, env):
         yield self.request
-        print("%s at Location of %d" % (self.name, self.position))
+        print("Atom %s at Location of %d" % (self.id, self.position))
+        self.log()
         yield env.timeout(1)
         while True:
             num_neighbor = self.getNumNeighbor()
             hop_interval = self.getHopInterval(num_neighbor)
             if num_neighbor == 2:# inland atom never hop
-                print("%s at Location of %d not at edge. No move" % (self.name, self.position))
+                print("Atom %s at Location of %d not at edge. No move" % (self.id, self.position))
                 yield env.timeout(1)
                 continue
 
             yield env.timeout(hop_interval)
-            print("%s at Location of %d" % (self.name, self.position))
+            print("Atom %s at Location of %d" % (self.id, self.position))
             # get next hopping position
             next_position = self.getNextPosition()
             # if next_position == self.position:
-            #     print('%s hop to same location' % (self.name))
+            #     print('%s hop to same location' % (self.id))
             #     continue
             # #
             if self.field.locations[next_position].count != 0:
-                print("Collision! %s tries to hop to Location %d but occupied. No move" % (self.name, next_position))
+                print("Collision! Atom %s tries to hop to Location %d but occupied. No move" % (self.id, next_position))
             else:
                 self.field.locations[self.position].release(self.request)
                 self.request = self.field.locations[next_position].request()
                 yield self.request
-                # print('%s request hopping to Location of %d' % (self.name, next_position))
                 self.position = next_position
-                print('%s hops to Location of %d.' % (self.name, self.position))
-
-def setup(env, scope_size, num_atom):
-    clock = 0
+                print('Atom %s hops to Location of %d.' % (self.id, self.position))
+                self.log()
+    def log(self):
+        self.monitor.data[env.now][self.id] = self.position
+def setup(env, monitor, scope_size, num_atom):
     field = Field(env, scope_size)
     for i in range(num_atom):
-        env.process(Atom('Atom %d' % i, field).run(env))
+        env.process(Atom(i, field, monitor).run(env))
 
     # Create more cars while the simulation is running
     # Create more atom as net deposition rate
     while True:
         yield env.timeout(1)
-        clock += 1
-        print("clock: %d", clock)
+        print("clock: %d" % env.now)
 
 
 # Setup and start the simulation
@@ -106,7 +106,8 @@ random.seed(RANDOM_SEED)  # This helps reproducing the results
 
 # Create an environment and start the setup process
 env = simpy.Environment()
-env.process(setup(env, SCOPE_SIZE, NUM_ATOM))
+monitor = Monitor(SIM_TIME, NUM_ATOM)
+env.process(setup(env, monitor, SCOPE_SIZE, NUM_ATOM))
 
 # Execute!
 env.run(until=SIM_TIME)
