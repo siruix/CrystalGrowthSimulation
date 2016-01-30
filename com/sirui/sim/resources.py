05f48f926190ce42fa7365aec71eaf939118916c
Sirui <sirui.xing@gmail.com>
2016-01-30 15:41:49 -0600

    A running 2D scenario

--- com/sirui/sim/resources.py
+++ com/sirui/sim/resources.py
@@ -1,11 +1,17 @@
 import simpy
-class Location(object):
+class Site(object):
     def __init__(self, env, atom = None):
         self.resource = simpy.Resource(env)
         self.atom = atom
 
 class Field(object):
+    # Simulation Scope
+    # 2D plane simple cube
+    # TODO 2D plane hex
     def __init__(self, env, scope_size):
         self.env = env
         # one piece of resource for each location
-        self.locations = [Location(env) for i in range(scope_size)]
+        self.sites = [[Site(env) for i in range(scope_size)] for j in range(scope_size)]
+
+    def getSite(self, position):
+        return self.sites[position.x][position.y]
