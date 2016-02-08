4dda0e06e8f9b3f2ec35278cefa0b1613e2ea026
Sirui <sirui.xing@gmail.com>
2016-02-08 13:05:50 -0600

    added closest vicinity attraction for ad-atom and dimmer

--- com/sirui/analytic/driver.py
+++ com/sirui/analytic/driver.py
@@ -7,9 +7,10 @@ from com.sirui.analytic.log_parser import LogParser
 import com.sirui.sim.TwoDSim as TwoDSim
 import shutil
 import os
-beta_phi = [5.0, 7.0]
+import logging
+beta_phi = [0.2, 0.5]
 # beta_phi = [5.0, 7.0, 10.0, 13.0, 15.0, 18.0, 20.0]
-beta_delta_mu = [5.0, 7.0]
+beta_delta_mu = [0.1]
 
 print('Cleaning all previous logs and images...')
 if os.path.exists('images'):
@@ -21,6 +22,6 @@ os.makedirs('logs')
 for x in beta_phi:
     for y in beta_delta_mu:
         for i in range(1):
-            TwoDSim.main(x, y, i)
+            TwoDSim.main(x, y, i, logging.INFO)
             parser = LogParser(x, y, i)
             parser.printFrames()
